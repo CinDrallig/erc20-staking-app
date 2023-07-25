af3efcfe48647a94b83e47a79b9d7d9745ec0f63
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-07-25 20:35:36 +0100

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,2 +1,14 @@
-// Replace this with your staking contract address
-export const stakingContractAddress = "0x9fB6514bc5048C92c87f4d25371325758f30592A";
+import { Pulsechain } from "@thirdweb-dev/chains";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK(Pulsechain, {
+  clientId: "YOUR_CLIENT_ID",
+});
+// --- OR ---
+// If used on the BACKEND pass your 'secretKey'
+const sdk = new ThirdwebSDK(Pulsechain, {
+  secretKey: "YOUR_SECRET_KEY",
+});
+
+const contract = await sdk.getContract("0x1D4F18d768755C19E036055fC5E597AC6740088a");
