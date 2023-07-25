735dc3e4b9335002291827945581a4993a069083
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-07-25 20:27:26 +0100

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -3,12 +3,12 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 // If used on the FRONTEND pass your 'clientId'
 const sdk = new ThirdwebSDK(Pulsechain, {
-  clientId: "YOUR_CLIENT_ID",
+  clientId: "fb5b78b67cf8fd462d6adb08086cc98b",
 });
 // --- OR ---
 // If used on the BACKEND pass your 'secretKey'
 const sdk = new ThirdwebSDK(Pulsechain, {
-  secretKey: "YOUR_SECRET_KEY",
+  secretKey: "mCicoRCbEq0C_ZaoQKLiHiA9YSsp-kClhMQveKnSoJbVZvC6jvCunNWhNAnQXYae0KtrQqnMeHSIKPxF5mi36Q",
 });
 
 const contract = await sdk.getContract("0x1D4F18d768755C19E036055fC5E597AC6740088a");
