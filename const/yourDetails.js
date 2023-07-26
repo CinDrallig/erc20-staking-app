e52516164fb6a612d693160ade7df8cb2d024b99
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-07-26 17:34:51 +0100

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,2 +1,3 @@
 // Replace this with your staking contract address
 export const stakingContractAddress = "0x1D4F18d768755C19E036055fC5E597AC6740088a";
+export const stakingContractAddress1 = "0x4eCeE519072271317B138a11C1FE990c8d99b2F6";
