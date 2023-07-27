0c1f7a184a7504f19453dd4f8168affb27745d6a
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-07-27 04:09:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
         <h1 className={styles.title}>Wolf Cola Co HAMS Staking </h1>
         <a href="https://app.pulsex.com/swap?outputCurrency=0x13342624b9D3049fB8Ef0a15b803f704864Bb844/">Official $HAMS Contract on Pulsechain</a>
         <p className={styles.description}>
-          Stake $HAMS, Earn $WOOF!
+          Stake $HAMS, Earn $WOOF! 1 HAMS = 100 WOLF
         </p>
 
         <div className={styles.connect}>
