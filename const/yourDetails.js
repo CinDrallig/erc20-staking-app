7b91a1fb97968e96bbc58d3e3f3c44d686c8fb82
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-07-29 01:05:00 +0100

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -3,5 +3,5 @@
 // export const stakingContractAddress = "0x5AcA766c7016A292e75E37e64961232e528fc164";
 // export const clientId = "e5cdf133d049b91666ef4d591daf95ec";
 
-export const stakingContractAddress = "0x1D4F18d768755C19E036055fC5E597AC6740088a";
+export const stakingContractAddress = "0xA72a116Fff7555D89F49d76945Bf5ee9cDDd1368";
 export const clientId = "fb5b78b67cf8fd462d6adb08086cc98b";
