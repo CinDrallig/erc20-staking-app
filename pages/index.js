f103c054a6dd46a2a818a8379a6462639fd5ff84
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-08-01 22:49:08 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -65,10 +65,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Wolf Cola Co HAMS Staking </h1>
-        <a href="https://app.pulsex.com/swap?outputCurrency=0x13342624b9D3049fB8Ef0a15b803f704864Bb844/">Official $HAMS Contract on Pulsechain</a>
+        <h1 className={styles.title}>Wolf Cola Co SONIC Staking </h1>
+        <a href="https://app.pulsex.com/swap?outputCurrency=0x13342624b9D3049fB8Ef0a15b803f704864Bb844/">Official $SONIC Contract on Pulsechain</a>
         <p className={styles.description}>
-          Stake $HAMS, Earn $WOOF!
+          Stake $SONIC, Earn $WOOF!
         </p>
         <a className={styles.description}>
           🐹 840000% APR 🐺
@@ -132,7 +132,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a className={styles.card}>
-            <h2>HAMS balance</h2>
+            <h2>SONIC balance</h2>
             <p>{stakingTokenBalance?.displayValue}</p>
           </a>
 
